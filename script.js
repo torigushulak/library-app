5842be6ab24395f143700601dd4ba802d5617e42
Tori Gushulak <torigushulak@Toris-MacBook-Air.local>
2022-08-29 21:46:15 -0600

    bugs fixed and css added

--- script.js
+++ script.js
@@ -2,32 +2,60 @@
 let myLibrary = [];
 
 class book {
-    constructor(author,title,pages) {
+    constructor(author, title, pages) {
         this.title = title,
         this.author = author,
         this.pages = pages
     }
 }
 
-let winnie = {title: "Title: Winnie the Pooh" , author: "Author: A.A.Milne", pages: "176 pages"};
-let cujo = {title: "Title: Cujo", author: "Author: Stephen King", pages: "400 pages"}
+// this code is the view "refresher"
+function render() {
+    myLibrary.forEach(book => {
+        // append to library element
+        library.innerHTML = '';
+        for (i = 0; i < myLibrary.length; i++) {
+            temp = document.createElement('div');
+            temp.className = 'book';
+            temp.innerHTML = myLibrary[i];
+            document.getElementsByTagName('div')[0].appendChild(temp);
+        }
+    });
+}
 
-myLibrary.push(winnie.title,winnie.author, winnie.pages);
+const winnie = {
+    title: "Title: Winnie the Pooh", 
+    author: "Author: A.A.Milne",
+    pages: "Pages: 176 " 
+};
+
+const cujo = { 
+    title: "Title: Cujo", 
+    author: "Author: Stephen King", 
+    pages: "Pages: 400 " 
+};
+
+myLibrary.push(winnie.title, winnie.author, winnie.pages);
 myLibrary.push(cujo.title, cujo.author, cujo.pages);
 
 
+
 let library = document.getElementById('library')
-library.append(myLibrary);
 
 let form = document.querySelector("form");
-form.onsubmit = function() {
+function handleForm(event) { event.preventDefault(); }
+form.addEventListener('submit', handleForm);
+
+form.onsubmit = function () {
     author = document.getElementById("author").value;
-    title = document.getElementById("title").value;  
-    pages = document.getElementById("pages").value;  
+    title = document.getElementById("title").value;
+    pages = document.getElementById("pages").value;
 
     newBook = new book(author, title, pages);
     myLibrary.push(newBook.title, newBook.author, newBook.pages);
-    library.append(myLibrary);
+
+    // reset dom
+    render ()
 }
 
 
@@ -35,10 +63,10 @@ form.onsubmit = function() {
 let newBookButton = document.getElementById('newbook');
 
 form.style.visibility = "hidden";
-newBookButton.addEventListener("click", function() {
+newBookButton.addEventListener("click", function () {
     const form = document.getElementById('form');
 
-    if(form.style.visibility === 'hidden') {
+    if (form.style.visibility === 'hidden') {
         // shows form
         form.style.visibility = 'visible';
     } else {
@@ -47,19 +75,6 @@ newBookButton.addEventListener("click", function() {
     }
 });
 
-// displays new book
-// const submitNewBook = getElementById('submit');
 
-// submitNewBook.addEventListener('click', function() {
-//     author = document.getElementById("author").value;
-//     title = document.getElementById("title").value;  
-//     pages = document.getElementById("pages").value;  
-
-//     let newBook = new book(author, title, pages);
-//     myLibrary.push(newBook.title, newBook.author, newBook.pages);
-//     library.append(myLibrary);
-
-// });
-
-
+window.onload = () => render();
 
